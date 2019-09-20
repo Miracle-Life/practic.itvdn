69baeaad3bae38bde8e92b462ff108ddc1464f0e
Denis <belovdenis777@gmail.com>
2019-09-20 11:25:24 +0300

    routing v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
+import { AboutComponent } from './content/about/about.component';
+import { PageNotFoundComponent } from './content/page-not-found/page-not-found.component';
+import {ContentComponent} from "./content/content.component";
+import { ContactsComponent } from './content/contacts/contacts.component';
+import { GalleryComponent } from './content/gallery/gallery.component';
+import { HomeComponent } from './content/home/home.component';
+
 
 const routes: Routes = [
-  {path: 'app',component:AppComponent},
-  {path: 'header',component:HeaderComponent},
-  {path: 'footer',component: FooterComponent},
-  {path: 'contents',component:ContentComponent},
+  {path: '',component:ContentComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contacts',component:ContactsComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'home',component:HomeComponent},
+  {path:'**',component:PageNotFoundComponent}
 ];
 
 @NgModule({
   declarations:[],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { PageNotFoundComponent } from './content/page-not-found/page-not-found.component';
+import { AboutComponent } from './content/about/about.component';
+import { ContactsComponent } from './content/contacts/contacts.component';
+import { GalleryComponent } from './content/gallery/gallery.component';
+import { HomeComponent } from './content/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    PageNotFoundComponent,
+    AboutComponent,
+    ContactsComponent,
+    GalleryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
