4842807f32cc7ab4196ecb9999dff1a0e74aa5fa
Denis <belovdenis777@gmail.com>
2019-09-20 10:28:11 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import {CommonModule} from "@angular/common";
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'app',component:AppComponent},
+  {path: 'header',component:HeaderComponent},
+  {path: 'footer',component: FooterComponent},
+  {path: 'contents',component:ContentComponent},
+];
 
 @NgModule({
+  declarations:[],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
